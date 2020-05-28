997e458555ac8215d880662bc16bb92e381ef013
goodSirTimothy <timothyclifton52@gmail.com>
2020-05-28 14:56:15 -0500

    this is taking longer then it should

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -14,9 +16,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public static TimeEntryRepository inMemoryTimeEntryRepository() {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    public static TimeEntryRepository inMemoryTimeEntryRepository(DataSource dataSource) {
         return new JdbcTimeEntryRepository(dataSource);
     }
 }
